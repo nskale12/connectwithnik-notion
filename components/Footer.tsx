2276d3113080e8fc2e0ac43aeb5f801b90fe4a7b
Nik Kale <nikkal@cisco.com>
2022-07-25 20:41:27 -0700

    Final Build FALA

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>© {(new Date()).getFullYear()} Dabble in Tech Inc. All Rights Reserved.</div>
 
       <div className={styles.settings}>
         {hasMounted && (
