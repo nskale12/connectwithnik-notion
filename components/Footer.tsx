832d866f52557bc6124b98c8f85c55828d5f035b
Nik Kale <nikkal@cisco.com>
2022-07-25 20:43:43 -0700

    Final Build FALAA

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>© {(new Date()).getFullYear()} Dabble in Tech Inc. All Rights Reserved.</div>
+      <div className={styles.copyright}>© {(new Date()).getFullYear()} Dabble in Tech Inc</div>
 
       <div className={styles.settings}>
         {hasMounted && (
